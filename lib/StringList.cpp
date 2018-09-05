2140dc22781ed86ba7b7b33aa180dd422d91ed33
ceengen <42974405+ceengen@users.noreply.github.com>
2018-09-04 22:16:24 -0400

    Create StringList.cpp

---
+++ lib/StringList.cpp
@@ -0,0 +1,17 @@
+//constructor
+StringList()
+
+//destructor
+~StringList()
+
+//add a string to the tail of a list
+push_back(string)
+
+//return a copy of the n-th item in the list
+get(n)
+
+//get the number of items in the list
+length()
+
+//remove the head of the list and return a copy of the data that was removed
+remove_front()
