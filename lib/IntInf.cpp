3f245da97c6dee6e71f9c48ae179af460838765f
ceengen <42974405+ceengen@users.noreply.github.com>
2018-11-18 14:21:42 -0500

    Update IntInf.cpp

--- lib/IntInf.cpp
+++ lib/IntInf.cpp
@@ -24,3 +24,36 @@ IntInf IntInf::operator +(const IntInf& rhs) const{
 	}
 	return result;
 }
+
+IntInf IntInf::operator -(const IntInf& rhs) const{
+	IntInf result;
+	if(posInf && rhs.posInf) //if both +inf
+	{
+		result.posInf = true;
+	}
+	else if(rhs.posInf) //this not +inf and rhs is posInf
+	{
+		result.negInf = true;
+	}
+	else if(posInf) //this +inf, rhs !+inf
+	{
+		result.posInf = true;
+	}
+	else if(negInf && rhs.negInf) //both -inf
+	{
+		result.negInf = true;
+	}
+	else if(rhs.negInf) //this !negInf and rhs = -inf
+	{
+		result.posInf = true;
+	}
+	else if(negInf) //this -inf, rhs !-inf
+	{
+		result.negInf = true;
+	}
+	else //both integers
+	{
+		result.num = num + rhs.num;
+	}
+	return result;	
+}
