e8b39a10e632fff8c9983cbe95f289bd582beabe
ceengen <42974405+ceengen@users.noreply.github.com>
2018-11-18 14:50:35 -0500

    Update IntInf.h

--- include/IntInf.h
+++ include/IntInf.h
@@ -3,13 +3,14 @@
 #include <stdio.h>
 
 class IntInf{
-private:
+public:
 	int num;
 	bool negInf;
 	bool posInf;
+	bool undefined;
 
 public:
-	IntInf(int n=0, bool neg=false, bool pos=false);
+	IntInf(int n=0, bool neg=false, bool pos=false, bool undefined=false);
 	IntInf operator =(const IntInf& rhs) const;
 	IntInf operator +(const IntInf& rhs) const;
 	IntInf operator -(const IntInf& rhs) const;
@@ -20,4 +21,4 @@ public:
 	bool operator <(const IntInf& rhs) const;
 };
 
-#endif //EXAMPLE_H
+#endif 
