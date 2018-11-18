abfcff9ac2506502848afa32129bb160ec296af9
ceengen <42974405+ceengen@users.noreply.github.com>
2018-11-18 13:33:19 -0500

    Create IntInf.cpp

---
+++ lib/IntInf.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+#include <stdio.h>
+
+class IntInf{
+private:
+	int num;
+	bool negInf;
+	bool posInf;
+
+public:
+	IntInf();
+	IntInf& operator =(const IntInf& rhs) const;
+	IntInf& operator +(const IntInf& rhs) const;
+	IntInf& operator -(const IntInf& rhs) const;
+	IntInf& operator *(const IntInf& rhs) const;
+	IntInf& operator /(const IntInf& rhs) const;
+	bool operator ==(const IntInf& rhs) const;
+	bool operator >(const IntInf& rhs) const;
+	bool operator <(const IntInf& rhs) const;
+};
+
+IntInf& IntInf::operator +(const IntInf& rhs){
+	if(posInf || rhs.posInf) //either is +infinity
+	{
+		posInf = true;
+		negInf = false;
+	}
+	else if(negInf || rhs.negInf) //neither is +infinity and one is -infinity
+	{
+		posInf = false;
+		negInf = true;
+	}
+	else //both integers
+	{
+		num = num + rhs.num;
+	}
+	return this;
+}
