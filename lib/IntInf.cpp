21bfe727475b3541d1ff1e3b95536852a401ce94
ceengen <42974405+ceengen@users.noreply.github.com>
2018-11-18 13:55:36 -0500

    Update IntInf.cpp

--- lib/IntInf.cpp
+++ lib/IntInf.cpp
@@ -1,25 +1,5 @@
-#include <iostream>
-using namespace std;
-
+#include "IntInf.h"
 #include <stdio.h>
-
-class IntInf{
-private:
-	int num;
-	bool negInf;
-	bool posInf;
-
-public:
-	IntInf(int n=0, bool neg=false, bool pos=false);
-	IntInf operator =(const IntInf& rhs) const;
-	IntInf operator +(const IntInf& rhs) const;
-	IntInf operator -(const IntInf& rhs) const;
-	IntInf operator *(const IntInf& rhs) const;
-	IntInf operator /(const IntInf& rhs) const;
-	bool operator ==(const IntInf& rhs) const;
-	bool operator >(const IntInf& rhs) const;
-	bool operator <(const IntInf& rhs) const;
-};
 
 IntInf::IntInf(int n, bool neg, bool pos)
 {
