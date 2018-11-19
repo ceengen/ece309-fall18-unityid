a11e9c5deea1428a2ecacefb143d4e704586242a
ceengen <42974405+ceengen@users.noreply.github.com>
2018-11-18 23:04:18 -0500

    Update shortestPath.cpp

--- lib/shortestPath.cpp
+++ lib/shortestPath.cpp
@@ -165,3 +165,134 @@ bool doesPathExist(Graph &g, int *path, int length) {
   }
   return true;
 }
+
+class MinHeapNode {
+public:
+  int distance;
+  int val;
+  int pred;
+  
+  MinHeapNode()
+  {
+    distance = INT_MAX;
+    pred = -1;
+};
+
+
+
+class MinHeap {
+private:
+  int *heapArray;    // this is the maxheap
+  int heapArraySize; // size of the array
+  int nextIndex;     // location of next free array entry
+
+protected:
+  void percolate_up(int);
+  void percolate_down(int, int);
+
+public:
+  MinHeap(int size = 100) : heapArraySize(size) {
+    heapArray = new int[size];
+    for (int i = 0; i < size; i++)
+      heapArray[i] = 0;
+    nextIndex = 0;
+  }
+
+  void print() {
+    for (int i = 0; i < nextIndex; i++) {
+      printf("%d ", heapArray[i]);
+    }
+    printf("\n");
+  }
+
+  bool insert(int);
+  bool remove(int &); // remove the max value
+
+private:
+  MinHeap(int *array, int size, int length = 0)
+      : heapArray(array), heapArraySize(size), nextIndex(length) {
+    int m = nextIndex / 2 - 1; // last internal BTNode
+    while (m >= 0) {
+      percolate_down(m, length);
+      m--;
+    }
+  }
+
+public:
+  static void sort(int *array, int size);
+};
+
+void MinHeap::percolate_up(int index) {
+  while (index > 0) {
+    int parent_index = (index - 1) / 2;
+    if (heapArray[index] < heapArray[parent_index]) {
+      int tmp = heapArray[index];
+      heapArray[index] = heapArray[parent_index];
+      heapArray[parent_index] = tmp;
+      index = parent_index;
+    } else
+      return;
+  }
+}
+
+void MinHeap::percolate_down(int index, int size) {
+  int child_index = index * 2 + 1;
+  int val = heapArray[index];
+  while (child_index < size) {
+    int min_val = val;
+    int min_index = -1;
+    for (int i = child_index; i < child_index + 2 && i < size; i++) {
+      if (heapArray[i] < max_val) {
+        min_val = heapArray[i];
+        min_index = i;
+      }
+    }
+    if (min_val == val)
+      return;
+    heapArray[index] = min_val;
+    heapArray[min_index] = val;
+    index = min_index;
+    child_index = index * 2 + 1;
+  }
+}
+
+bool MinHeap::insert(int data) {
+  if (nextIndex < heapArraySize) {
+    heapArray[nextIndex] = data;
+    percolate_up(nextIndex);
+    nextIndex++;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool MinHeap::remove(int &val) {
+  if (nextIndex > 0) {
+    val = heapArray[0];
+    heapArray[0] = heapArray[nextIndex - 1];
+    nextIndex -= 1;
+    percolate_down(0, nextIndex);
+    return true;
+  }
+  return false;
+}
+
+void MinHeap::sort(int *array, int size) {
+  MinHeap heap(array, size, size);
+
+  printf("\nheapify: ");
+  for (int i = 0; i < 10; i++) {
+    printf("%d ", array[i]);
+  }
+
+  for (int i = size - 1; i > 0; i--) {
+    int ref;
+    heap.remove(ref);
+    array[i] = ref;
+  }
+
+  // return, and the array passed in is sorted
+}
+
+
